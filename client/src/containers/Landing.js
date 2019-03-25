b08060d112a29adda16b03a960f4aabdec4b6a82
Dustin McGilvray <dustinmcgilvray@gmail.com>
2019-03-25 11:04:20 -0500

    Removed Container from Landing Page.

--- client/src/containers/Landing.js
+++ client/src/containers/Landing.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
-import Container from 'react-bootstrap/Container';
 import StartGameButton from "../components/StartGameButton/StartGame";
 import Carousel from "../components/Carousel/Carousel";
 import Footer from "../components/Footer/Footer";
 class Landing extends Component {
     render() {
         return (
-        <Container fluid="true">
-        <Carousel/>
-        <StartGameButton/>
-        <Footer/>     
-        </Container>
+        <div>
+            <Carousel/>
+            <StartGameButton/>
+            <Footer/>     
+        </div>
         )      
     }
 };
